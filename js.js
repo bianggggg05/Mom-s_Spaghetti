298939d9549664209745907131ce7c3382399065
Bianca G. Calingacion <131579232+bianggggg05@users.noreply.github.com>
2024-01-26 13:55:06 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    const toggleMenu = document.querySelector(".toggleMenu");
+    const navigation = document.querySelector(".navigation");
+    toggleMenu.classList.toString.toggle("active");
+    navigation.classList.toString.toggle("active");
+    
+
+}
